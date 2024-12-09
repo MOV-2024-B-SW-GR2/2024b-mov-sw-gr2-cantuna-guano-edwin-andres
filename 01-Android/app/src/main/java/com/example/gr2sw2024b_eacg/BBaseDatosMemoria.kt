c8675016a27ebf6342ced23b624a067b7635e562
Cantx08 <edwin.cantuna@epn.edu.ec>
2024-12-09 12:30:03 -0500

    update: 00-Kotlin & 01-Android files

---
+++ 01-Android/app/src/main/java/com/example/gr2sw2024b_eacg/BBaseDatosMemoria.kt
@@ -0,0 +1,12 @@
+package com.example.gr2sw2024b_eacg
+
+class BBaseDatosMemoria {
+    companion object {
+        var arregloBEntrenador = arrayListOf<BEntrenador>()
+        init {
+            arregloBEntrenador.add(BEntrenador(1, "Andres", "a@a.com"))
+            arregloBEntrenador.add(BEntrenador(1, "Edwin", "b@b.com"))
+            arregloBEntrenador.add(BEntrenador(1, "Carolina", "c@c.com"))
+        }
+    }
+}
